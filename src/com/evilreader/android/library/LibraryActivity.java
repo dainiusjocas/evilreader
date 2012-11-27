b714a4e87a27e7193403bab4caa8447877507b04
Dainius Jocas <dainiusjocas@gmail.com>
2012-11-27 14:32:08 +0100

    refresh library

--- src/com/evilreader/android/library/LibraryActivity.java
+++ src/com/evilreader/android/library/LibraryActivity.java
@@ -1,9 +1,12 @@
 package com.evilreader.android.library;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,17 +16,30 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.Toast;
 
-import com.evilreader.android.MainActivity;
 import com.evilreader.android.R;
+import com.evilreader.android.dbcontroller.DBAdapter;
 
 public class LibraryActivity extends Activity {
+	/* Database integration */
+	private DBAdapter mDbAdapter;
+	
+	/* Evil Library Manager */
+	private EvilLibraryManager _EvilLibraryManager;
 	
 	private static Context context;
+	private Bundle _SavedInstanceState;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// set or create base directory for ebooks
 		super.onCreate(savedInstanceState);
+		this._SavedInstanceState = savedInstanceState;
+		// set or create base directory for ebooks
+		this._EvilLibraryManager = EvilLibraryManager.getInstance(this);
+		ArrayList<String> fileNames = new ArrayList<String>();
+		fileNames = this._EvilLibraryManager.getListOfEvilBooksFromDB();
+		// TODO(dainius): code which handles the situation when no books are available
+		ArrayList<String> test = new ArrayList<String>();
+		test.addAll(fileNames);
 		LibraryActivity.context = getApplicationContext();
 		setContentView(R.layout.activity_library);
 		// Impossible to set fullscreen in layout xml file, so it is done here
@@ -31,14 +47,23 @@ public class LibraryActivity extends Activity {
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		GridView gridview = (GridView) findViewById(R.id.gridview);
-		gridview.setAdapter(new ImageAdapter(this));
+		gridview.setAdapter(new TextAdapter(this, test));
+		
 		gridview.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	            Toast.makeText(LibraryActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+	        }
+	    });
+		
+		//gridview.setAdapter(new ImageAdapter(this));
+		
+/*		gridview.setOnItemClickListener(new OnItemClickListener() {
 	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
 	        	Intent mIntent = new Intent(LibraryActivity.this, com.evilreader.android.epubcontentcontrol.EpubContentActivity.class);
 				startActivity(mIntent);
 	            //Toast.makeText(LibraryActivity.this, "" + position, Toast.LENGTH_SHORT).show();
 	        }
-	    });
+	    });*/
 
 	}
 	
@@ -72,12 +97,26 @@ public class LibraryActivity extends Activity {
         	 displayEvilMessage("Search for Evil Books!");
              return true;
          case R.id.refresh_library:
+        	 // TODO(dainius): scan evil library, put books to the db, fill the grid, show the grid
         	 // refresh button is pressed
-        	 displayAllImages(); // make visible images
+        	 this._EvilLibraryManager.refreshListOfEvilBooks();
+        	 this.displayEvilBooks();
+        	 //displayAllImages(); // make visible images
         	 displayEvilMessage("Refresh Evil Library!");
         	 return true;
          }
          return false;
+	}
+	
+	public void displayEvilBooks() {
+		GridView gridview = (GridView) findViewById(R.id.gridview);
+		gridview.invalidateViews();
+		ArrayList<String> mEvilBooks = 
+				this._EvilLibraryManager.getListOfEvilBooksFromDB();
+		TextAdapter mTextAdapter = new TextAdapter(this, mEvilBooks);
+		mTextAdapter.notifyDataSetChanged();
+		gridview.setAdapter(mTextAdapter);
+		gridview.setVisibility(View.VISIBLE);
 	}
 	
 	/**
