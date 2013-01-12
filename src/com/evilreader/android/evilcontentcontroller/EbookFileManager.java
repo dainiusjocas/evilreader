81a61ba62f2de4a08b8924b9730db0e940eced8f
ele <hipogriff@ubuntu.(none)>
2013-01-12 15:53:00 +0100

    problematic controller

---
+++ src/com/evilreader/android/evilcontentcontroller/EbookFileManager.java
@@ -0,0 +1,181 @@
+package com.evilreader.android.evilcontentcontroller;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import nl.siegmann.epublib.domain.Book;
+import nl.siegmann.epublib.domain.Spine;
+import nl.siegmann.epublib.domain.SpineReference;
+import nl.siegmann.epublib.epub.EpubReader;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.Uri;
+
+public class EbookFileManager {
+	// The singleton instance of this manager
+	private static final EbookFileManager INSTANCE = new EbookFileManager();
+	static int Width;
+	static int Height;
+	static int FontSize;
+	static int LettersPerPageConstraint = 500;
+	static int LinesPerPageConstraint;
+
+	private Context contextWrapper;
+	private ArrayList<String> currentEbookContentWithinPages;
+	// if the index is equal to -1 that means that there is no content in the
+	// EBookContent
+	private int currentEBookContentIndex;
+
+	// private constructor for Singleton purpose
+	private EbookFileManager() {
+
+	}
+	
+	// provides logic for calculating how much letters the control,
+	// containing the content of the currently loaded book, should visualise without
+	// needing to show scroll control for the user.
+	private void InitLettersPerPageCount() {
+		
+	}
+	
+	private void InitiateEBookContent(Book book, int progress) {
+		currentEbookContentWithinPages = new ArrayList<String>();
+		currentEBookContentIndex = progress;
+		InputStream inputStream = null;
+		Scanner scanner = null;
+				
+		//Spine spine = null;
+		
+		try {
+			int currentPageLetterCount = 0;
+			int newPageCreatedFlag = 0;
+			String buffer = "";
+			
+			for (SpineReference spineReference : book.getSpine()
+					.getSpineReferences()) {
+				inputStream = spineReference.getResource().getInputStream();
+				scanner = new Scanner(inputStream, "UTF-8")
+						.useDelimiter("</p>");
+				;
+				while (scanner.hasNext()) {
+					String currentContent = scanner.next();
+					
+					if(currentContent.length() <= LettersPerPageConstraint/2)
+					{
+						buffer += currentContent;
+					}
+					
+					if(buffer.length() >= LettersPerPageConstraint){
+						currentEbookContentWithinPages.add(buffer);
+						buffer = "";
+					}
+					/*	
+					if(newPageCreatedFlag == 0 && contentForLoading.length() != 0) {
+						//currentPageLetterCount
+					}
+						
+					currentEbookContentWithinPages.add(contentForLoading);*/
+				}
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+
+	public static EbookFileManager getInstance() {
+		return EbookFileManager.INSTANCE;
+	}
+
+	// The contextWrapper as a reference is needed to have access to the local
+	// storage of the Application,
+	// so this class needs this data
+	public void Init(Context ContextWrapper) {
+		contextWrapper = ContextWrapper;
+	}
+
+	// Method for downloading new epub books to the local resource storage of
+	// the Application and load their content
+	@SuppressWarnings("unused")
+	public void LoadEpubBookByUrl(Uri url) {
+		// TODO(Viktor): Implement
+	}
+
+	// Method for loading the content from the pointed file
+	@SuppressWarnings("unused")
+	public void LoadEpubBookByAbsolutePath(String filePath) {
+		// TODO(Viktor): Implement
+	}
+
+	// Method used for loading epub files from the Raw resources.
+	// Used only for a testing purpose for the implemented functionality in this
+	// class ...
+	@SuppressLint("UseSparseArrays")
+	public void LoadEpubBookByRawResourceID(int resourceID) {
+		try {
+			// Obsolete
+			InputStream inputStream = contextWrapper.getResources()
+					.openRawResource(resourceID);
+
+			Book currentEBook = inputStream != null ? (new EpubReader())
+					.readEpub(inputStream) : null;
+
+			if (currentEBook != null) {
+				this.InitiateEBookContent(currentEBook, -1);
+			} else {
+				return;
+			}
+
+		} catch (Exception e) {
+
+		}
+	}
+
+	@SuppressWarnings("unused")
+	public String GetFirstPage() {
+		if (currentEbookContentWithinPages != null) {
+			currentEBookContentIndex = 0;
+
+			return currentEbookContentWithinPages.get(currentEBookContentIndex);
+		}
+
+		return "";
+	}
+
+	@SuppressWarnings("unused")
+	public String GetLastPage() {
+		if (currentEbookContentWithinPages != null) {
+			currentEBookContentIndex = currentEbookContentWithinPages.size() - 1;
+
+			return currentEbookContentWithinPages.get(currentEBookContentIndex);
+		}
+
+		return "";
+	}
+
+	public String GetNextPage() {
+		currentEBookContentIndex++;
+		if (currentEbookContentWithinPages != null
+				&& currentEbookContentWithinPages.size() > currentEBookContentIndex) {
+
+			return currentEbookContentWithinPages.get(currentEBookContentIndex);
+		}
+
+		return "";
+	}
+
+	public String GetPreviousPage() {
+		currentEBookContentIndex--;
+		if (currentEbookContentWithinPages != null
+				&& currentEBookContentIndex >= 0) {
+
+			return currentEbookContentWithinPages.get(currentEBookContentIndex);
+		}
+
+		return "";
+	}
+
+}
---
+++ src/com/evilreader/android/evilcontentcontroller/EpubContentActivity.java
@@ -0,0 +1,121 @@
+package com.evilreader.android.evilcontentcontroller;
+
+import com.evilreader.android.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.ClipboardManager;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class EpubContentActivity extends Activity {
+	// private constants
+	// TODO(Viktor): in the future we'll use more sophisticated techniques
+	private final int resourceIDSample = R.raw.asd;
+
+	// private members
+	final String javaScriptLibraries = "<html><head><script src='../../assets/jquery.js'></script><script src='../../assets/rangy-core.js'></script><script src='../../assets/rangy-serializer.js'></script><script src='../../assets/android.selection.js'></script></head><body>";
+	private EvilreaderWebView webviewPage1;
+	private TextView textView1;
+
+	private int currentChapterIndex;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_epub_content);
+
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		webviewPage1 = (EvilreaderWebView) findViewById(R.id.webView);
+		
+		//String filePath = "file:///android_asset/content.html";
+		//webviewPage1.loadUrl(filePath);
+		
+		int width = webviewPage1.getWidth();
+		int height = webviewPage1.getHeight();
+		WebSettings webViewPage1Settings = webviewPage1.getSettings();
+		// webViewPage1Settings.get
+		int fontSize = webViewPage1Settings.getDefaultFontSize();
+
+		// textView1 = (TextView) findViewById(R.id.textView1);
+
+		EbookFileManager ebookFileManagerInstance = EbookFileManager
+				.getInstance();
+		ebookFileManagerInstance.Init(getApplicationContext());
+		EbookFileManager.Height = height;
+		EbookFileManager.Width = width;
+		EbookFileManager.FontSize = fontSize;
+
+		try {
+			ebookFileManagerInstance
+					.LoadEpubBookByRawResourceID(resourceIDSample);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// The button for going through chapters forward
+		final Button button1 = (Button) findViewById(R.id.button1);
+		button1.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				currentChapterIndex++;
+
+				String chapterContent = EbookFileManager.getInstance()
+						.GetNextPage();
+
+				if (chapterContent != null || chapterContent != "") {
+
+					webviewPage1.setClickable(true);
+					webviewPage1.loadData(javaScriptLibraries + chapterContent + "</body></html>", "text/html", null);
+
+					// textView1.setText(chapterContent);
+				}
+			}
+		});
+
+		// The button for going through chapters backward
+		final Button button2 = (Button) findViewById(R.id.button2);
+		button2.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				if (currentChapterIndex > 0) {
+					currentChapterIndex--;
+
+					String chapterContent = EbookFileManager.getInstance()
+							.GetPreviousPage();
+
+					if (chapterContent != null || chapterContent != "") {
+
+						webviewPage1.setClickable(true);
+						webviewPage1
+								.loadData(javaScriptLibraries + chapterContent+ "</body></html>", "text/html", null);						
+					}
+				}
+
+			}
+		});
+		
+		webviewPage1.loadData(javaScriptLibraries + ebookFileManagerInstance.GetFirstPage()+ "</body></html>",
+				"text/html", "UTF-8");
+	}
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_epub_content, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+}
