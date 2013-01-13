fd5440589e6f7eca2db1cf77b32f36c1b9bc8712
Dainius Jocas <dainiusjocas@gmail.com>
2013-01-13 21:21:21 +0200

    Saving things in the DB

---
+++ src/com/evilreader/android/evilcontentcontroller/GemManager.java
@@ -0,0 +1,162 @@
+package com.evilreader.android.evilcontentcontroller;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+
+import com.evilreader.android.dbcontroller.DBAdapter;
+
+/**
+ * This class is for saving bookmarks, highlights and and notes to the db.
+ * @author user
+ *
+ */
+public class GemManager {
+	// Context that we are executing
+	private Context _Context;
+	// Database controller
+	private DBAdapter _DBAdapter;
+	
+	/**
+	 * Contructor
+	 * @param pContext
+	 */
+	public GemManager(Context pContext) {
+		this._Context = pContext;
+		this._DBAdapter = new DBAdapter(this._Context);
+	}
+	
+	/**
+	 * Puts note and its location to the database
+	 * @param pNote
+	 * @param pBookId
+	 * @param pChapter
+	 * @param pParagraph
+	 */
+	public void storeNote(String pNote, String pBookId, String pChapter, 
+			String pParagraph) {
+		this._DBAdapter.open();
+		this._DBAdapter.storeNote(pNote, pBookId, pChapter, pParagraph);
+		this._DBAdapter.close();
+	}
+	
+	/**
+	 * Gets string representation of all notes
+	 * TODO(dainius): now it is not much useful. Just for testing purposes
+	 * @param pBookId
+	 * @return
+	 */
+	public String getNotes(String pBookId) {
+		String notes = "";
+		this._DBAdapter.open();
+		Cursor aCursor = this._DBAdapter.fetchAllNotes(pBookId);
+		if (!aCursor.moveToFirst()) {
+			aCursor.close();
+			this._DBAdapter.close();
+			return "No notes for this book";
+		}
+		do {
+			notes = notes + 
+					aCursor.getString(0)
+					+ " "
+					+ aCursor.getString(1) 
+					+ " "
+					+ aCursor.getString(2)
+					+ " "
+					+ aCursor.getString(3);
+			notes = notes + "\n";
+		} while(aCursor.moveToNext());
+		aCursor.close();
+		this._DBAdapter.close();
+		return notes;
+	}
+	
+	/**
+	 * Puts bookmark into the database
+	 * @param pBookId
+	 * @param pChapter
+	 * @param pParagraph
+	 */
+	public void saveBookmark(String pBookId, String pChapter, 
+			String pParagraph) {
+		this._DBAdapter.open();
+		this._DBAdapter.storeBookmark(pBookId, pChapter, pParagraph);
+		this._DBAdapter.close();
+	}
+	
+	/**
+	 * Gets text representation of all the bookmarks from db
+	 * TODO(dainius): redo
+	 * @param pBookId
+	 * @return
+	 */
+	public String getBookmarks(String pBookId) {
+		String bookmarks = "";
+		this._DBAdapter.open();
+		Cursor aCursor = this._DBAdapter.fetchBookmarks(pBookId);
+		if (!aCursor.moveToFirst()) {
+			aCursor.close();
+			this._DBAdapter.close();
+			return "No Bookmarks for this book";
+		}
+		do {
+			bookmarks = bookmarks + aCursor.getString(0)
+					+ " "
+					+ aCursor.getString(1) 
+					+ " "
+					+ aCursor.getString(2);
+			bookmarks = bookmarks + "\n";
+		} while(aCursor.moveToNext());
+		aCursor.close();
+		this._DBAdapter.close();
+		return bookmarks;
+	}
+	
+	/**
+	 * Saves highlighted text and its location in the database
+	 * @param pHighlightedText
+	 * @param pBookId
+	 * @param pChapter
+	 * @param pParagraph
+	 */
+	public void saveHighlight(String pHighlightedText, String pBookId,
+			String pChapter, String pParagraph) {
+		this._DBAdapter.open();
+		this._DBAdapter.storeHighlight(pBookId, pChapter, pParagraph, 
+				pHighlightedText);
+		this._DBAdapter.close();
+	}
+	
+	/**
+	 * Gets string representation of all the highlights stored in db
+	 * Cursor is to those elements:
+	 *   {HIGHLIGHT_BOOK_ID, HIGHLIGHT_CHAPTER, HIGHLIGHT_PARAGRAPH, 
+			HIGHLIGHT_TEXT};
+	 * @param pBookId
+	 * @return
+	 */
+	public String getHighlights(String pBookId) {
+		String highlights = "";
+		this._DBAdapter.open();
+		Cursor aCursor = this._DBAdapter.fetchHighlights(pBookId);
+		if (!aCursor.moveToFirst()) {
+			aCursor.close();
+			this._DBAdapter.close();
+			return "No highlights for this book";
+		}
+		do {
+			highlights = highlights + aCursor.getString(0)
+					+ " "
+					+ aCursor.getString(1) 
+					+ " "
+					+ aCursor.getString(2)
+					+ " "
+					+ aCursor.getString(3);
+			highlights = highlights + "\n";
+		} while(aCursor.moveToNext());
+		aCursor.close();
+		this._DBAdapter.close();
+		return highlights;
+	}
+
+}
